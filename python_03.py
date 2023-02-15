b2b932d31283de9efa122b8aaf3376bc663dbbcc
silviacastilloj <silvia.castillo.juarez@gmail.com>
2023-02-14 20:19:16 -0600

    Practica 03

---
+++ python_03.py
@@ -0,0 +1,16 @@
+nombre = input("Cuál es tu nombre?")
+print(nombre)
+
+edad = input("Cuál es tu edad?")
+print(edad)
+
+ciudad = input("Cuál es tu Ciudad?")
+print(ciudad)
+
+numero_1 = input("Ingresa un número entre 1-100")
+print(numero_1)
+
+numero_2 = input("Ingresa un número entre 100-200")
+print(numero_2)
+
+print (f "Mi nombre es {Nombre}, mi edad es {edad}, soy de la ciudad de {cuidad}")
